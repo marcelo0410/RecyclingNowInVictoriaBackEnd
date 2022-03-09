d9d0b7a15e4537bc7bc4bb22dca7a0702194221f
marcelo0410 <cs95401156@gmail.com>
2022-03-10 04:53:23 +0800

    solve CORS problem and bug fix

--- src/main/java/com/monash/recyclinginvic/config/CorsConfig.java
+++ src/main/java/com/monash/recyclinginvic/config/CorsConfig.java
@@ -9,10 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("/*")
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600)
-                .allowedHeaders("*");
+                .allowedHeaders("/*")
+                .exposedHeaders("Header1", "Header2");
     }
 }
