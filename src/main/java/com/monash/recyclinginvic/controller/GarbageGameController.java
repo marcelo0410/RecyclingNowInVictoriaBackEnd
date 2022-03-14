c539d472baf65c9ecda232da3d8a87b75b3cebcd
marcelo0410 <cs95401156@gmail.com>
2022-03-14 13:22:58 +0800

    Minor fix: restructure code and remove redundant methods

--- src/main/java/com/monash/recyclinginvic/controller/GarbageGameController.java
+++ src/main/java/com/monash/recyclinginvic/controller/GarbageGameController.java
@@ -19,38 +19,11 @@ public class GarbageGameController {
     @Autowired
     private GarbageGenreService garbageGenreService;
 
-    @GetMapping("/hello")
-    public String hello1(){
-        return "Hello world!";
-    }
-
-    @GetMapping("/hello2")
-    public String hello2(){
-        return "Hello world!";
-    }
-
-    @GetMapping("/json")
-    public Map<String, Object> hello3(){
-        Map<String, Object> map = new HashMap<>();
-        map.put("key1", "value1");
-        map.put("results", "123");
-        return map;
-    }
-
     @GetMapping("findAll")
     public List<GarbageGenre> findAllGarbage() {
-        //调用service的方法
         List<GarbageGenre> list = garbageGenreService.list();
         return list;
     }
 
-    // search a keyword
-//    @GetMapping("/search/{name}")
-//    public List<GarbageGenre> searchKeyWord(@PathVariable String name){
-//        QueryWrapper<GarbageGenre> wrapper = new QueryWrapper<>();
-//        wrapper.like("name", name);
-//        List<GarbageGenre> garbageGenres = garbageGenreService.getBaseMapper().selectList(wrapper);
-//        return garbageGenres;
-//    }
 }
 
